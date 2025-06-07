aa41708eb87efa3d9277a5e04e606181bb2cb153
Pranay Dharamsare <158022096+pranayd17@users.noreply.github.com>
2025-06-07 23:48:32 +0530

    Create Delete columns to make sorted III.cpp

---
+++ Delete columns to make sorted III.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        
+        int n = strs.size();
+        int m = strs[0].length();
+
+        vector<int> dp(m, 1);
+        int res = 1;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < i; j++) {
+                for(int k = 0; k <= n; k++) {
+                    if(k == n) {
+                        dp[i] = max(dp[i], dp[j] + 1);
+                        res = max(res, dp[i]);
+                    } else if (strs[k][j] > strs[k][i]) {
+                        break;
+                    }
+                }
+            }
+        }
+        return m - res;
+    }
+};
