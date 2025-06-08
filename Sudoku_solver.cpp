51de284ba451c715df79b6464e99b5f2cf4f9a98
Pranay Dharamsare <158022096+pranayd17@users.noreply.github.com>
2025-06-08 23:26:48 +0530

    Update Sudoku_solver.cpp

--- Sudoku_solver.cpp
+++ Sudoku_solver.cpp
@@ -1,28 +1,40 @@
-bool check(vector<vector<char>> &board, int i, int j, char val)
-{
-    int row = i - i%3, column = j - j%3;
-    for(int x=0; x<9; x++) if(board[x][j] == val) return false;
-    for(int y=0; y<9; y++) if(board[i][y] == val) return false;
-    for(int x=0; x<3; x++)
-    for(int y=0; y<3; y++)
-        if(board[row+x][column+y] == val) return false;
-    return true;
-}
-bool solveSudoku(vector<vector<char>> &board, int i, int j)
-{
-    if(i==9) return true;
-    if(j==9) return solveSudoku(board, i+1, 0);
-    if(board[i][j] != '.') return solveSudoku(board, i, j+1);
-
-    for(char c='1'; c<='9'; c++)
-    {
-        if(check(board, i, j, c))
-        {
-            board[i][j] = c;
-            if(solveSudoku(board, i, j+1)) return true;
-            board[i][j] = '.';
+class Solution {
+private:
+    bool isValid(vector<vector<char>>& board, int row, int col, char ch){
+        for(int i=0; i<9; i++){
+            if(board[i][col] == ch){
+                return false;
+            }
+            if(board[row][i] == ch){
+                return false;
+            }
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == ch){
+                return false;
+            }
         }
+        return true;
     }
-        
-    return false;
-}
+    bool solve(vector<vector<char>>& board) {
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
+                if(board[i][j] == '.'){
+                    for(char ch='1'; ch<='9'; ch++){
+                        if(isValid(board, i, j, ch)){
+                            board[i][j] = ch;
+                            if(solve(board) == true){
+                                return true;
+                            }
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
